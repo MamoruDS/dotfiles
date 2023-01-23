4381d8ba53018755d573e9207bb6aafdfcb6d5f4
MamoruDS <mamoruds.io@gmail.com>
2023-01-23 15:07:12 +0900

    improved: zsh-fzf-history-search

--- zsh/plugins/configs/fzf_history_search.zsh
+++ zsh/plugins/configs/fzf_history_search.zsh
@@ -1,6 +1,10 @@
 {{#if (eq zsh_plugin_fzf_history_search_preview 1)}}
-ZSH_FZF_HISTORY_SEARCH_FZF_ARGS='+s +m -x -e'
-ZSH_FZF_HISTORY_SEARCH_FZF_EXTRA_ARGS='--preview-window=down:30%'
+src=~/.local/share/zinit/plugins/joshskidmore---zsh-fzf-history-search/zsh-fzf-history-search.zsh
+if [ -f $src ]; then
+    sed -i "s/FZF_EXTRA_ARGS} -q/FZF_EXTRA_ARGS} --preview 'format-history-z {}' -q/" $src
+    ZSH_FZF_HISTORY_SEARCH_FZF_ARGS='+s +m -x -e'
+    ZSH_FZF_HISTORY_SEARCH_FZF_EXTRA_ARGS='--preview-window=down:30%'
+fi
 {{/if}}
 ZSH_FZF_HISTORY_SEARCH_END_OF_LINE=1
 ZSH_FZF_HISTORY_SEARCH_EVENT_NUMBERS=0
