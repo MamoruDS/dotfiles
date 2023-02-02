bdfafb7333a436b047b0793d151aa9e693d560a4
MamoruDS <mamoruds.io@gmail.com>
2023-02-02 17:37:51 +0900

    fixed: local patch for `zsh-fzf-history` now working in macos

--- zsh/plugins/configs/fzf_history_search.zsh
+++ zsh/plugins/configs/fzf_history_search.zsh
@@ -1,7 +1,11 @@
 {{#if (eq zsh_plugin_fzf_history_search_preview 1)}}
 src=~/.local/share/zinit/plugins/joshskidmore---zsh-fzf-history-search/zsh-fzf-history-search.zsh
 if [ -f $src ]; then
-    sed -i "s/FZF_EXTRA_ARGS} -q/FZF_EXTRA_ARGS} --preview 'format-history-z {}' -q/" $src
+    if [ "$(uname)" = "Darwin" ]; then
+        sed -i .bak "s/FZF_EXTRA_ARGS} -q/FZF_EXTRA_ARGS} --preview 'format-history-z {}' -q/" $src
+    else
+        sed -i "s/FZF_EXTRA_ARGS} -q/FZF_EXTRA_ARGS} --preview 'format-history-z {}' -q/" $src
+    fi
     ZSH_FZF_HISTORY_SEARCH_FZF_ARGS='+s +m -x -e'
     ZSH_FZF_HISTORY_SEARCH_FZF_EXTRA_ARGS='--preview-window=down:30%'
 fi
