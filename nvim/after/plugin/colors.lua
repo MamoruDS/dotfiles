fac9682393e3ae78f9b39e5707a00b49ade868be
MamoruDS <mamoruds.io@gmail.com>
2023-02-02 17:54:23 +0900

    added: `after/plugin/colors.lua` for package `nvim`

---
+++ nvim/after/plugin/colors.lua
@@ -0,0 +1,6 @@
+require("github-theme").setup({
+    theme_style = "dark_default",
+    function_style = "italic",
+    sidebars = {"qf", "vista_kind", "terminal", "packer"},
+    transparent = true
+})
--- nvim/lua/mamoruds/plugin_config.lua
+++ nvim/lua/mamoruds/plugin_config.lua
@@ -1,10 +1,3 @@
-require("github-theme").setup({
-    theme_style = "dark_default",
-    function_style = "italic",
-    sidebars = {"qf", "vista_kind", "terminal", "packer"},
-    transparent = true
-})
-
 {{#if nvim_node_host_prog}}
 vim.g.node_host_prog = "{{nvim_node_host_prog}}"
 {{/if}}
