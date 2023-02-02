97793b312a69fe171d6526aae156ba3e406e6e8c
MamoruDS <mamoruds.io@gmail.com>
2023-02-02 17:56:27 +0900

    improved: better format

--- env.sh
+++ env.sh
@@ -2,18 +2,21 @@
 
 {{#if is_executable "vim"}}
 export EDITOR=vim
+
 {{/if}}
 {{#if is_executable "nvim"}}
 export EDITOR=nvim
-{{/if}}
 
+{{/if}}
 {{#if is_executable "bat"}}
 export PAGER=bat
+
 {{/if}}
 {{#if is_executable "nvimpager"}}
 export PAGER=nvimpager
-{{/if}}
 
+{{/if}}
 {{#if (or (is_executable "gpg") (is_executable "gpg2"))}}
 export GPG_TTY=$(tty)
+
 {{/if}}
