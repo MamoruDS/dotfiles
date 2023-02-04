f6a148956a32c362081fa756e37b42e79f23f065
MamoruDS <mamoruds.io@gmail.com>
2023-02-04 19:31:33 +0900

    added: dotfile for starship

--- zsh/after/theme.zsh
+++ zsh/after/theme.zsh
@@ -1,3 +1,16 @@
+{{#if (eq zsh_theme "starship")}}
+if [ -x "$(command -v starship)" ]; then
+    eval "$(starship init zsh)"
+    if [ -z "$TMUX" -a ! "$TERM_PROGRAM" = "vscode" ]; then
+        export STARSHIP_CONFIG=~/.config/starship/default.toml
+    else
+        export STARSHIP_CONFIG=~/.config/starship/less.toml
+    fi
+else
+    echo "[DOTFILES] starship not found, installing..."
+    curl -sS https://starship.rs/install.sh | sh
+fi
+{{/if}}
 {{#if (eq zsh_theme "spaceship")}}
 ZSH_THEME="spaceship"
 {{/if}}
