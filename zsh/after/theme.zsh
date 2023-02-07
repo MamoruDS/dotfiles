a26eb1e53936cc290667fdf4a39d05a65d31d125
MamoruDS <mamoruds.io@gmail.com>
2023-02-07 12:45:58 +0900

    improved: how starship install & init

--- zsh/after/theme.zsh
+++ zsh/after/theme.zsh
@@ -1,15 +1,21 @@
 {{#if (eq zsh_theme "starship")}}
-if [ -x "$(command -v starship)" ]; then
-    eval "$(starship init zsh)"
+_init_starship() {
+    if [ ! "$STARSHIP_SHELL" = "zsh" ]; then
+        eval "$(starship init zsh)"
+    fi
     if [ -z "$TMUX" -a ! "$TERM_PROGRAM" = "vscode" ]; then
         export STARSHIP_CONFIG=~/.config/starship/default.toml
     else
         export STARSHIP_CONFIG=~/.config/starship/less.toml
     fi
-else
+}
+
+if [ ! -x "$(command -v starship)" ]; then
     echo "[DOTFILES] starship not found, installing..."
     curl -sS https://starship.rs/install.sh | sh
 fi
+
+_init_starship
 {{/if}}
 {{#if (eq zsh_theme "spaceship")}}
 ZSH_THEME="spaceship"
