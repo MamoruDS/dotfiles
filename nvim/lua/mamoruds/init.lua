8be16ae81c0b1b76a14aa9c43c26c946d53c983d
MamoruDS <mamoruds.io@gmail.com>
2023-02-07 21:36:45 +0900

    modified: moves nvim options from `lua/mamoruds/init.lua` -> `lua/mamoruds/options.lua`

--- nvim/lua/mamoruds/init.lua
+++ nvim/lua/mamoruds/init.lua
@@ -1,21 +1,5 @@
-vim.opt.number = true
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
-
-vim.opt.autoread = true
-vim.opt.background = "dark"
-vim.opt.cursorline = true
-vim.opt.fileencoding = "utf-8"
-vim.opt.mouse = "a"
-vim.opt.updatetime = 250
-
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-
-vim.opt.termguicolors = true
-
 require("mamoruds.providers")
+require("mamoruds.options")
 require("mamoruds.plugins")
 
 vim.api.nvim_command('set ft=conf')
---
+++ nvim/lua/mamoruds/options.lua
@@ -0,0 +1,24 @@
+vim.opt.number = false
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.autoread = true
+vim.opt.background = "dark"
+vim.opt.cursorline = true
+vim.opt.fileencoding = "utf-8"
+vim.opt.mouse = "a"
+vim.opt.updatetime = 250
+
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+vim.opt.termguicolors = true
+
+-- coc
+vim.opt.backup = false
+vim.opt.writebackup = false
+vim.opt.signcolumn = "yes"
+
+-- global variables
+vim.g.notify_timeout = 5000
