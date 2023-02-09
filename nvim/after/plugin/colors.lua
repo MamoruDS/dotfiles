eecf9f27aec47e11dc1f834137d5ed5cbb547cac
MamoruDS <mamoruds.io@gmail.com>
2023-02-09 22:58:58 +0900

    updated: border highlight for nvim

--- nvim/after/plugin/colors.lua
+++ nvim/after/plugin/colors.lua
@@ -1,6 +1,8 @@
 if vim.opt.background:get() == 'dark' then
     vim.cmd[[
         hi NormalFloat  ctermbg=None    guibg=None
+        hi FloatBorder  ctermfg=8       guifg=#32363d    
+        hi VertSplit    ctermfg=8       guifg=#32363d
     ]]
 else
     vim.cmd[[
