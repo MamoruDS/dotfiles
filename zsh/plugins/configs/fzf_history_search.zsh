3c00255c13717eb28d0a04b3e6d38d6b0912bb2a
MamoruDS <mamoruds.io@gmail.com>
2023-02-12 00:07:49 +0900

    updated: targets preview-window patch to temp zsh-fzf-history-search

--- zsh/plugins/configs/fzf_history_search.zsh
+++ zsh/plugins/configs/fzf_history_search.zsh
@@ -1,5 +1,6 @@
 {{#if (eq zsh_plugin_fzf_history_search_preview 1)}}
-src=~/.local/share/zinit/plugins/joshskidmore---zsh-fzf-history-search/zsh-fzf-history-search.zsh
+# src=~/.local/share/zinit/plugins/joshskidmore---zsh-fzf-history-search/zsh-fzf-history-search.zsh
+src=~/.local/share/zinit/plugins/MamoruDS---zsh-fzf-history-search/zsh-fzf-history-search.zsh
 if [ -f $src ]; then
     if [ "$(uname)" = "Darwin" ]; then
         sed -i .bak "s/FZF_EXTRA_ARGS} -q/FZF_EXTRA_ARGS} --preview 'format-history-z {}' -q/" $src
