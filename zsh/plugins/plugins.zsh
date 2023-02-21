199a7a894a8c97ea2293edf5e151dd663c48c9ad
MamoruDS <mamoruds.io@gmail.com>
2023-02-21 15:24:14 +0900

    Update history fuzzy search to `MamoruDS/zsh-history-fuzzy-search`

--- zsh/plugins/plugins.zsh
+++ zsh/plugins/plugins.zsh
@@ -1,8 +1,5 @@
 {{#if (is_executable "fzf")}}
 zinit light Aloxaf/fzf-tab
-# zinit light joshskidmore/zsh-fzf-history-search
-zinit ice ver"multiline"
-zinit light MamoruDS/zsh-fzf-history-search
 
 {{/if}}
 {{#if (eq zsh_theme "spaceship")}}
@@ -10,6 +7,7 @@ zinit light spaceship-prompt/spaceship-prompt
 
 {{/if}}
 zinit light zdharma-continuum/fast-syntax-highlighting
+zinit light MamoruDS/zsh-history-fuzzy-search
 
 {{#if (eq zsh_plugin_autosuggestions 1)}}
 zinit light zsh-users/zsh-autosuggestions
