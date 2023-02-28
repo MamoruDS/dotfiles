aab5dfa9952b6c46d77e4ecd386d1d15a7eb9b71
MamoruDS <mamoruds.io@gmail.com>
2023-02-28 16:35:19 +0900

    Fix zsh's keybinds issue (hopefully :(

--- zsh/configs/keybinds.zsh
+++ zsh/configs/keybinds.zsh
@@ -5,22 +5,19 @@ autoload -Uz up-line-or-beginning-search down-line-or-beginning-search
 zle -N up-line-or-beginning-search
 zle -N down-line-or-beginning-search
 
-bindkey -e # using emacs mode; https://wiki.archlinux.org/title/Zsh#Key_bindings
+bindkey -e # using emacs mode
 
-bindkey "^[[1;5C" forward-word
-bindkey "^[[1;5D" backward-word
+bindkey "\e[1;5C" forward-word
+bindkey "\e[1;5D" backward-word
 
-# TODO:
-if [ "$(uname)" = "Darwin" ]; then
-    # bindkey -- "${terminfo[khome]}" beginning-of-line
-    bindkey "^[[H" beginning-of-line
-    # bindkey -- "${terminfo[kend]}" end-of-line
-    bindkey "^[[F" end-of-line
-    bindkey "^[[A" up-line-or-beginning-search
-    bindkey "^[[B" down-line-or-beginning-search
-else
-    [ -n "$key[Home]" ] && bindkey -- "$key[Home]" beginning-of-line
-    [ -n "$key[End]" ] && bindkey -- "$key[End]" end-of-line
-    [ -n "$key[Up]" ] && bindkey -- "$key[Up]" up-line-or-beginning-search
-    [ -n "$key[Down]" ] && bindkey -- "$key[Down]" down-line-or-beginning-search
-fi
+[ -n "$key[Home]" ] && bindkey -- "$key[Home]" beginning-of-line
+bindkey "\e[H" beginning-of-line
+
+[ -n "$key[End]" ] && bindkey -- "$key[End]" end-of-line
+bindkey "\e[F" end-of-line
+
+[ -n "$key[Up]" ] && bindkey -- "$key[Up]" up-line-or-beginning-search
+bindkey "\e[A" up-line-or-beginning-search
+
+[ -n "$key[Down]" ] && bindkey -- "$key[Down]" down-line-or-beginning-search
+bindkey "\e[B" down-line-or-beginning-search
