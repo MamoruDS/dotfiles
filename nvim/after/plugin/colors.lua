50c47a1de20d8788b7f0be9b5268b0a7e0f613d4
MamoruDS <mamoruds.io@gmail.com>
2023-03-02 22:11:24 +0900

    style: bg modify for group `Search`

--- nvim/after/plugin/colors.lua
+++ nvim/after/plugin/colors.lua
@@ -4,7 +4,8 @@ vim.cmd [[
 
 if vim.opt.background:get() == 'dark' then
     vim.cmd [[
-        hi FloatBorder  ctermfg=8       guifg=#32363d    
+        hi FloatBorder  ctermfg=8       guifg=#32363d
+        hi Search       guibg=#867647
         hi VertSplit    ctermfg=8       guifg=#32363d
     ]]
 end
