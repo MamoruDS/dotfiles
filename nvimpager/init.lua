d851bbcff3268aa8755102a55cdcef59ac24165c
MamoruDS <mamoruds.io@gmail.com>
2023-03-07 15:40:54 +0900

    rewrite: `nvimpager/init.lua`

--- nvimpager/init.lua
+++ nvimpager/init.lua
@@ -1,12 +1,20 @@
 vim.opt.termguicolors = true
 
-vim.api.nvim_exec(
-    'set runtimepath+=~/.local/share/nvim/site/pack/packer/start/github-nvim-theme/',
-    true)
+local plugin_root = vim.fn.stdpath("data") .. "/site/pack/packer/start/"
+local plugins = {
+    'github-nvim-theme',
+}
+for _, plugin in ipairs(plugins) do
+    vim.o.runtimepath = vim.o.runtimepath .. ',' .. plugin_root .. plugin .. '/'
+end
 
 require("github-theme").setup({
+    {{#if (eq nvim_background "dark")}}
     theme_style = "dark_default",
+    {{/if}}
+    {{#if (eq nvim_background "light")}}
+    theme_style = "light",
+    {{/if}}
     function_style = "italic",
-    sidebars = { "qf", "vista_kind", "terminal", "packer" },
     transparent = true,
 })
