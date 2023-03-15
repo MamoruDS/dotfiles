9c8212a51251099ac99422989a94f98740ba1338
MamoruDS <mamoruds.io@gmail.com>
2023-03-16 02:22:40 +0900

    refactor: avoid use keyword `local`

--- install.sh
+++ install.sh
@@ -13,9 +13,9 @@ error() {
 }
 
 test_writeable() {
-    fp="$1/.test.dotfiles"
-    if touch "$fp" 2> /dev/null; then
-        rm "$fp"
+    _fp="$1/.test.dotfiles"
+    if touch "$_fp" 2> /dev/null; then
+        rm "$_fp"
         return 0
     else
         return 1
@@ -23,42 +23,42 @@ test_writeable() {
 }
 
 get_valid_path() {
-    local e='echo '"$1"''
-    echo "$(eval $e)"
+    _path_eval='echo '"$1"''
+    echo "$(eval $_path_eval)"
 }
 
 get_dotter_bin_dir() {
-    local default_dotter_bin_dir='/usr/local/bin'
-    read -p "enter the install dir for dotter ($default_dotter_bin_dir): " dotter_bin_dir
-    echo $(get_valid_path ${dotter_bin_dir:-$default_dotter_bin_dir})
+    _default_dotter_bin_dir='/usr/local/bin'
+    read -p "enter the install dir for dotter ($_default_dotter_bin_dir): " dotter_bin_dir
+    echo $(get_valid_path ${dotter_bin_dir:-$_default_dotter_bin_dir})
 }
 
 get_dotfiles_root() {
-    local default_dotfiles_root=~/.dotfiles
-    read -p "enter the root of dotfiles ($default_dotfiles_root): " dotfiles_root
-    echo $(get_valid_path ${dotfiles_root:-$default_dotfiles_root})
+    _default_dotfiles_root=~/.dotfiles
+    read -p "enter the root of dotfiles ($_default_dotfiles_root): " dotfiles_root
+    echo $(get_valid_path ${dotfiles_root:-$_default_dotfiles_root})
 }
 
 get_dotfiles_local() {
-    local default_dotfiles_local=$DOTFILES_ROOT/.dotter/local.toml
-    read -p "enter the path of local configuration ($default_dotfiles_local): " dotfiles_local
-    echo $(get_valid_path ${dotfiles_local:-$default_dotfiles_local})
+    _default_dotfiles_local=$DOTFILES_ROOT/.dotter/local.toml
+    read -p "enter the path of local configuration ($_default_dotfiles_local): " dotfiles_local
+    echo $(get_valid_path ${dotfiles_local:-$_default_dotfiles_local})
 }
 
 try_install_dotter() {
-    local dotter_dl_dir=/tmp
+    _dotter_dl_dir=/tmp
     if [ $(uname -s) = 'Linux' ] && [ $(uname -m) = 'x86_64' ]; then
         DOTTER_BIN_DIR=$(get_dotter_bin_dir)
         info "downloading dotter from github release"
-        curl -L https://github.com/SuperCuber/dotter/releases/latest/download/dotter -o $dotter_dl_dir/dotter
-        chmod +x $dotter_dl_dir/dotter
+        curl -L https://github.com/SuperCuber/dotter/releases/latest/download/dotter -o $_dotter_dl_dir/dotter
+        chmod +x $_dotter_dl_dir/dotter
         info "installing dotter to $DOTTER_BIN_DIR"
         [ ! -d "$DOTTER_BIN_DIR" ] && (mkdir -p "$DOTTER_BIN_DIR" 2> /dev/null )
         if test_writeable "$DOTTER_BIN_DIR"; then
-            mv $dotter_dl_dir/dotter "$DOTTER_BIN_DIR"
+            mv $_dotter_dl_dir/dotter "$DOTTER_BIN_DIR"
         else
             warn "permission is required to install dotter to $DOTTER_BIN_DIR"
-            sudo mv $dotter_dl_dir/dotter "$DOTTER_BIN_DIR"
+            sudo mv $_dotter_dl_dir/dotter "$DOTTER_BIN_DIR"
         fi
     else
         error "unsupported platform $(uname -s) $(uname -m), please rerun this script after installing dotter manually, see https://github.com/SuperCuber/dotter#others"
@@ -67,29 +67,29 @@ try_install_dotter() {
 }
 
 clone_repo() {
-    local repo="$1"
-    local dir="$2"
-    if [ -d "$dir" ]; then
-        warn "$dir already exists, skip cloning"
+    _repo="$1"
+    _dir="$2"
+    if [ -d "$_dir" ]; then
+        warn "$_dir already exists, skip cloning"
         return 0
     else
-        git clone -q "$repo" "$dir"
+        git clone -q "$_repo" "$_dir"
     fi
 }
 
 generate_preset_local() {
-    local local_fp="$1"
+    _local_fp="$1"
 
-    if [ ! -d "$(dirname $local_fp)" ]; then
-        mkdir -p "$(dirname $local_fp)"
+    if [ ! -d "$(dirname $_local_fp)" ]; then
+        mkdir -p "$(dirname $_local_fp)"
     fi
 
-    if [ -f "$local_fp" ]; then
-        warn "$local_fp already exists, skip generating"
+    if [ -f "$_local_fp" ]; then
+        warn "$_local_fp already exists, skip generating"
         return 0
     fi
     
-    cat << EOF > $local_fp
+    cat << EOF > $_local_fp
 packages = ["tmux", "zsh"]
 
 [files]
@@ -116,14 +116,14 @@ EOF
 }
 
 check_requirements() {
-    local missing=""
+    _missing=""
 
     if [ ! -x "$(command -v curl)" ]; then
-        missing="$missing curl"
+        _missing="$_missing curl"
     fi
 
     if [ ! -x "$(command -v git)" ]; then
-        missing="$missing git"
+        _missing="$_missing git"
     fi
 
     if [ -x "$(command -v dotter)" ]; then
@@ -131,7 +131,7 @@ check_requirements() {
     elif [ -n "$DOTTER_BIN" ]; then
         if [ ! -x "$DOTTER_BIN" ]; then
             error '$DOTTER_BIN is not executable'
-            missing="$missing dotter"
+            _missing="$_missing dotter"
         fi
     else
         info "trying to install dotter"
