cff2b46389aa012f41f97ce30e6e6a934c06541d
MamoruDS <mamoruds.io@gmail.com>
2023-04-19 04:39:53 +0900

    update: github-theme setup to refactored config

--- nvim/lua/mamoruds/plugins/github-theme.lua
+++ nvim/lua/mamoruds/plugins/github-theme.lua
@@ -1,33 +1,29 @@
 -- https://github.com/projekt0n/github-nvim-theme#configuration
 
-{{#if (eq nvim_background "dark")}}
 require("github-theme").setup {
-    theme_style = "dark_default",
-    function_style = "italic",
-    sidebars = {"qf", "vista_kind", "terminal", "packer"},
-    {{#if (eq nvim_transparent_bg 1)}}
-    transparent = true
-    {{/if}}
+    options = {
+        styles = {
+            functions = "italic",
+        },
+        darken = {
+            sidebar = {
+                list = {"qf", "vista_kind", "terminal", "packer"},
+            } ,
+        },
+        {{#if (eq nvim_transparent_bg 1)}}
+        transparent = true,
+        {{/if}}
+    }
 }
+{{#if (eq nvim_background "dark")}}
 vim.cmd[[
     hi LineNr       ctermbg=None    ctermfg=240 guifg=#5d5f61
     hi CursorLine   ctermbg=8       guibg=#292e36
+    colorscheme github_dark_default
 ]]
-{{else}}
-{{#if (eq nvim_background "light")}}
-require("github-theme").setup {
-    theme_style = "light",
-    function_style = "italic",
-    sidebars = {"qf", "vista_kind", "terminal", "packer"},
-    {{#if (eq nvim_transparent_bg 1)}}
-    transparent = true
-    {{/if}}
-}
-{{else}}
-require("github-theme").setup {
-    {{#if (eq nvim_transparent_bg 1)}}
-    transparent = true
-    {{/if}}
-}
 {{/if}}
+{{#if (eq nvim_background "light")}}
+vim.cmd[[
+    colorscheme github_light
+]]
 {{/if}}
