38acd5134ab8a7fef504c20abb353c0c2aae9081
MamoruDS <mamoruds.io@gmail.com>
2023-05-23 17:27:42 +0900

    improve: listing only directories on ls

--- zsh/configs/completions.zsh
+++ zsh/configs/completions.zsh
@@ -9,3 +9,7 @@ _comp_options+=(globdots)
 zstyle ':completion:*' matcher-list 'm:{[:lower:]}={[:upper:]}' '+m:{[:upper:]}={[:lower:]}'
 zstyle ':completion:*' sort false
 zstyle ':completion:*' file-sort access
+
+# only show directories when completing with ls(and its aliases)
+zstyle ':completion:*:ls:*'  file-patterns '*(/):directories'
+zstyle ':completion:*:exa:*' file-patterns '*(/):directories'
