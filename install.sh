c60c748a662ac45a0d56300f0404b15216d00315
MamoruDS <mamoruds.io@gmail.com>
2023-12-11 04:15:05 +0900

    update: dotter bin url from github release (>=0.13.1

--- install.sh
+++ install.sh
@@ -83,11 +83,12 @@ get_packages() {
 
 try_install_dotter() {
     _dotter_dl_dir=/tmp
+    # TODO: aarch64
     if [ $(uname -s) = 'Linux' ] && [ $(uname -m) = 'x86_64' ]; then
         # set DOTTER_BIN_DIR
         get_dotter_bin_dir
         info "downloading dotter from github release"
-        curl -L https://github.com/SuperCuber/dotter/releases/latest/download/dotter -o $_dotter_dl_dir/dotter
+        curl -L https://github.com/SuperCuber/dotter/releases/latest/download/dotter-linux-x64-musl -o $_dotter_dl_dir/dotter
         chmod +x $_dotter_dl_dir/dotter
         info "installing dotter to $DOTTER_BIN_DIR"
         [ ! -d "$DOTTER_BIN_DIR" ] && (mkdir -p "$DOTTER_BIN_DIR" 2> /dev/null )
