5b9bb851e61f8f8028abc1095f7d0a504e12eada
MamoruDS <mamoruds.io@gmail.com>
2024-01-04 00:08:21 +0900

    update: `zsh/configs/keybinds` bindings for emacs only

--- zsh/configs/keybinds.zsh
+++ zsh/configs/keybinds.zsh
@@ -5,21 +5,29 @@ autoload -Uz up-line-or-beginning-search down-line-or-beginning-search
 zle -N up-line-or-beginning-search
 zle -N down-line-or-beginning-search
 
-bindkey -e # using emacs mode
+# keybind_helper() {
+#     local mode=$1
+#     local key=$2
+#     local action=$3
+#     [[ "$mode" = *"v"* ]] && bindkey -v $key $action
+#     [[ "$mode" = *"e"* ]] && bindkey -e $key $action
+#     [[ "$mode" = *"a"* ]] && bindkey -a $key $action
+# }
+# unset -f keybind_helper
 
-bindkey "\e[1;5C" forward-word
-bindkey "\e[1;5D" backward-word
+bindkey -e "\e[1;5C" forward-word
+bindkey -e "\e[1;5D" backward-word
 
-[ -n "$key[Home]" ] && bindkey -- "$key[Home]" beginning-of-line
-bindkey "\e[H" beginning-of-line
+[ -n "$key[Home]" ] && bindkey -e -- "$key[Home]" beginning-of-line
+bindkey -e "\e[H" beginning-of-line
 
-[ -n "$key[End]" ] && bindkey -- "$key[End]" end-of-line
-bindkey "\e[F" end-of-line
+[ -n "$key[End]" ] && bindkey -e -- "$key[End]" end-of-line
+bindkey -e "\e[F" end-of-line
 
-[ -n "$key[Up]" ] && bindkey -- "$key[Up]" up-line-or-beginning-search
-bindkey "\e[A" up-line-or-beginning-search
-bindkey "\eOA" up-line-or-beginning-search
+[ -n "$key[Up]" ] && bindkey -e -- "$key[Up]" up-line-or-beginning-search
+bindkey -e "\e[A" up-line-or-beginning-search
+bindkey -e "\eOA" up-line-or-beginning-search
 
-[ -n "$key[Down]" ] && bindkey -- "$key[Down]" down-line-or-beginning-search
-bindkey "\e[B" down-line-or-beginning-search
-bindkey "\eOB" down-line-or-beginning-search
+[ -n "$key[Down]" ] && bindkey -e -- "$key[Down]" down-line-or-beginning-search
+bindkey -e "\eOB" down-line-or-beginning-search
+bindkey -e "\e[B" down-line-or-beginning-search
