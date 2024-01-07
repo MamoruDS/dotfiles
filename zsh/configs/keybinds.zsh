6f3a24dd8a5838f5468edb1ee14d3edb357df503
MamoruDS <mamoruds.io@gmail.com>
2024-01-07 23:44:34 +0900

    update: add emacs key bindings only in non-vi mode

--- zsh/configs/keybinds.zsh
+++ zsh/configs/keybinds.zsh
@@ -5,15 +5,8 @@ autoload -Uz up-line-or-beginning-search down-line-or-beginning-search
 zle -N up-line-or-beginning-search
 zle -N down-line-or-beginning-search
 
-# keybind_helper() {
-#     local mode=$1
-#     local key=$2
-#     local action=$3
-#     [[ "$mode" = *"v"* ]] && bindkey -v $key $action
-#     [[ "$mode" = *"e"* ]] && bindkey -e $key $action
-#     [[ "$mode" = *"a"* ]] && bindkey -a $key $action
-# }
-# unset -f keybind_helper
+{{#if (ne zsh_vi 1)}}
+bindkey -e
 
 bindkey -e "\e[1;5C" forward-word
 bindkey -e "\e[1;5D" backward-word
@@ -31,3 +24,4 @@ bindkey -e "\eOA" up-line-or-beginning-search
 [ -n "$key[Down]" ] && bindkey -e -- "$key[Down]" down-line-or-beginning-search
 bindkey -e "\eOB" down-line-or-beginning-search
 bindkey -e "\e[B" down-line-or-beginning-search
+{{/if}}
