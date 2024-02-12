9d8ab9533d6ff2e79b7097d7e85a590dbd8490b0
MamoruDS <mamoruds.io@gmail.com>
2024-02-12 23:46:29 +0900

    update: migrate from `packer.nvim` to `lazy.nvim`

--- nvim/lua/mamoruds/plugins.lua
+++ nvim/lua/mamoruds/plugins.lua
@@ -1,18 +1,15 @@
-local packer_bootstrap = false
-local packer_path = vim.fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
-if vim.fn.empty(vim.fn.glob(packer_path)) > 0 then
-    vim.fn.system {
-        'git',
-        'clone',
-        '--depth',
-        '1',
-        'https://github.com/wbthomason/packer.nvim.git',
-        packer_path
-    }
-    vim.o.runtimepath = vim.o.runtimepath .. ',' .. packer_path
-    packer_bootstrap = true
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
-
+vim.opt.rtp:prepend(lazypath)
 
 function _require(module_name)
     local default_prefix = 'mamoruds'
@@ -27,167 +24,140 @@ function _require(module_name)
     end
 end
 
-require('packer').startup {
-    function(use)
-        use 'wbthomason/packer.nvim'
-
-        use {
-            'numToStr/Comment.nvim',
-            after = 'nvim-notify',
-            config = function()
-                _require('plugins.comment')
-            end
-        }
-
-        use {
-            'rcarriga/nvim-notify',
-            config = function()
-                _require('plugins.nvim-notify')
-            end,
-        }
-
-        use {
-            'projekt0n/github-nvim-theme',
-            after = 'nvim-notify',
-            config = function()
-                _require('plugins.github-theme')
-            end,
-        }
-
-        use {
-            'nvim-lualine/lualine.nvim',
-            after = 'nvim-notify',
-            event = 'BufEnter',
-            config = function()
-                _require('plugins.lualine')
-            end,
-        }
-
-        use {
-            'lewis6991/gitsigns.nvim',
-            after = 'nvim-notify',
-            event = 'BufRead',
-            cond = function()
-                return vim.wo.diff ~= true
-            end,
-            config = function()
-                _require('plugins.gitsigns')
-            end,
-        }
-
-        {{#if (eq use_nerdfont 1)}}
-        use {
-            'nvim-tree/nvim-web-devicons',
-            after = 'nvim-notify',
-            config = function()
-                _require('plugins.nvim-web-devicons')
-            end,
-        }
-
-        {{/if}}
-        use {
-            'nvim-tree/nvim-tree.lua',
-            after = 'nvim-notify',
-            config = function()
-                _require('plugins.nvim-tree')
-            end,
-        }
-
-        use {
-            'lukas-reineke/indent-blankline.nvim',
-            after = 'nvim-notify',
-            config = function()
-                _require('plugins.indent_blankline')
-            end,
-        }
-
-        use {
-            'neoclide/coc.nvim',
-            after = 'nvim-notify',
-            branch = 'release',
-            cond = function()
-                return (
-                    (vim.fn.executable('node') == 1) or
-                    (vim.fn.empty(vim.g.node_host_prog) == 0)
-                ) and vim.wo.diff ~= true
-            end,
-            config = function()
-                _require('plugins.coc')
-            end,
-        }
-
-        use {
-            'gabrielelana/vim-markdown',
-            after = 'nvim-notify',
-        }
-
-        use {
-            'neoclide/jsonc.vim',
-            after = 'nvim-notify',
-        }
-
-        use {
-            'cespare/vim-toml',
-            after = 'nvim-notify',
-        }
-        {{#if nvim_python3_host_prog}}
-
-        use {
-            'numirias/semshi',
-            -- cond = function()
-            --     return (vim.fn.executable('python') == 1)
-            -- end,
-            ft = 'python',
-            run = ':UpdateRemotePlugins',
-        }
-        {{/if}}
-
-        use {
-            'ojroques/vim-oscyank',
-            after = 'nvim-notify',
-            branch = 'main',
-            config = function()
-                _require('plugins.oscyank')
-            end,
-        }
-
-        use {
-            'nvim-treesitter/nvim-treesitter',
-            run = function()
-                local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
-                ts_update()
-            end,
-        }
-
-        use {
-            'github/copilot.vim',
-            after = 'nvim-notify',
-            cond = function()
-                return (
-                    (vim.fn.executable('node') == 1) or
-                    (vim.fn.empty(vim.g.copilot_node_command) == 0)
-                ) and vim.wo.diff ~= true
-            end,
-            setup = function()
-                _require('plugins.copilot')
-            end,
-        }
-
-        if packer_bootstrap then
-            require('packer').sync()
+require("lazy").setup({
+    {
+        'numToStr/Comment.nvim',
+        config = function()
+            _require('plugins.comment')
         end
-    end,
-    config = {
-        display = {
-            open_fn = function()
-                return require('packer.util').float {
-                    border = 'single',
-                    anchor = 'NE',
-                    row = 1,
-                    col = vim.o.columns - 1,
-                    width = 50,
-                }
-            end
-        }
     },
-}
+
+    {
+        'rcarriga/nvim-notify',
+        lazy = false,
+        priority = 1000,
+        config = function()
+            _require('plugins.nvim-notify')
+        end,
+    },
+
+    {
+        'projekt0n/github-nvim-theme',
+        lazy = false,
+        priority = 100,
+        config = function()
+            _require('plugins.github-theme')
+        end,
+    },
+
+    {
+        'nvim-lualine/lualine.nvim',
+        event = 'BufEnter',
+        config = function()
+            _require('plugins.lualine')
+        end,
+    },
+
+    {
+        'lewis6991/gitsigns.nvim',
+        event = 'BufRead',
+        cond = function()
+            return vim.wo.diff ~= true
+        end,
+        config = function()
+            _require('plugins.gitsigns')
+        end,
+    },
+    {{#if (eq use_nerdfont 1)}}
+
+    {
+        'nvim-tree/nvim-web-devicons',
+        config = function()
+            _require('plugins.nvim-web-devicons')
+        end,
+    },
+    {{/if}}
+
+    {
+        'nvim-tree/nvim-tree.lua',
+        config = function()
+            _require('plugins.nvim-tree')
+        end,
+    },
+
+    {
+        'lukas-reineke/indent-blankline.nvim',
+        config = function()
+            _require('plugins.indent_blankline')
+        end,
+    },
+
+    {
+        'neoclide/coc.nvim',
+        branch = 'release',
+        cond = function()
+            return (
+                (vim.fn.executable('node') == 1) or
+                (vim.fn.empty(vim.g.node_host_prog) == 0)
+            ) and vim.wo.diff ~= true
+        end,
+        config = function()
+            _require('plugins.coc')
+        end,
+    },
+
+    {
+        'gabrielelana/vim-markdown',
+        ft = "markdown",
+    },
+
+    {
+        'neoclide/jsonc.vim',
+        ft = "jsonc",
+    },
+
+    {
+        'cespare/vim-toml',
+        ft = "toml",
+    },
+    {{#if nvim_python3_host_prog}}
+
+    {
+        'numirias/semshi',
+        -- cond = function()
+        --     return (vim.fn.executable('python') == 1)
+        -- end,
+        ft = 'python',
+        build = ':UpdateRemotePlugins',
+    },
+    {{/if}}
+
+    {
+        'ojroques/vim-oscyank',
+        branch = 'main',
+        config = function()
+            _require('plugins.oscyank')
+        end,
+    },
+
+    {
+        'nvim-treesitter/nvim-treesitter',
+        build = function()
+            local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+            ts_update()
+        end,
+    },
+
+    {
+        'github/copilot.vim',
+        cond = function()
+            return (
+                (vim.fn.executable('node') == 1) or
+                (vim.fn.empty(vim.g.copilot_node_command) == 0)
+            ) and vim.wo.diff ~= true
+        end,
+        init = function()
+            _require('plugins.copilot')
+        end,
+    },
+})
