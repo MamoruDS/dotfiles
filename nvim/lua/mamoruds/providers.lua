868819301351ef75e5d89a2ac13f4222adb6375b
MamoruDS <mamoruds.io@gmail.com>
2024-02-16 00:12:45 +0900

    remove: drop `nvim_python3_host_prog` variable from global config

--- nvim/lua/mamoruds/providers.lua
+++ nvim/lua/mamoruds/providers.lua
@@ -1,9 +1,6 @@
 -- {{#if nvim_node_host_prog}}
 vim.g.node_host_prog = "{{nvim_node_host_prog}}"
 -- {{/if}}
--- {{#if nvim_python3_host_prog}}
-vim.g.python3_host_prog = "{{nvim_python3_host_prog}}"
--- {{/if}}
 -- {{#if nvim_plugin_copilot_node_host}}
 vim.g.copilot_node_command = "{{nvim_plugin_copilot_node_host}}"
 -- {{/if}}
