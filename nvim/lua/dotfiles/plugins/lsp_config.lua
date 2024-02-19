d644d4daf3f980123c31ba99a1548a8c8ad2a28d
MamoruDS <mamoruds.io@gmail.com>
2024-02-19 17:49:28 +0900

    update: lsp config with doc highlight autocmd

--- nvim/lua/dotfiles/plugins/lsp_config.lua
+++ nvim/lua/dotfiles/plugins/lsp_config.lua
@@ -2,3 +2,19 @@ require("mason").setup()
 require("mason-lspconfig").setup({
   ensure_installed = {},
 })
+
+vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+  pattern = "*",
+  callback = function()
+    local opts = { silent = true }
+    vim.lsp.buf.document_highlight(opts)
+  end,
+})
+
+vim.api.nvim_create_autocmd("CursorMoved", {
+  pattern = "*",
+  callback = function()
+    local opts = { silent = true }
+    vim.lsp.buf.clear_references(opts)
+  end,
+})
