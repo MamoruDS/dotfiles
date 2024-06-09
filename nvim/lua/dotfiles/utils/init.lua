576cc779fcb032050707e9bd4258748f9f705ef2
MamoruDS <mamoruds.io@gmail.com>
2024-06-09 13:26:30 +0900

    add: utils func `isInTable`

--- nvim/lua/dotfiles/utils/init.lua
+++ nvim/lua/dotfiles/utils/init.lua
@@ -2,6 +2,15 @@ local M = {}
 
 M.require = require("dotfiles.utils.require")
 
+M.isInTable = function(tbl, value)
+  for _, v in ipairs(tbl) do
+    if v == value then
+      return true
+    end
+  end
+  return false
+end
+
 M.mergeTables = function(t1, t2)
   for k, v in pairs(t2) do
     t1[k] = v
