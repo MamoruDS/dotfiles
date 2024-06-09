66eaa3be8535783d94dd1c90ce5640b5f954c7de
MamoruDS <mamoruds.io@gmail.com>
2024-06-09 14:05:34 +0900

    update: func `utils.mergeTables` with deep merge

--- nvim/lua/dotfiles/utils/init.lua
+++ nvim/lua/dotfiles/utils/init.lua
@@ -12,9 +12,16 @@ M.isInTable = function(tbl, value)
 end
 
 M.mergeTables = function(t1, t2)
-  for k, v in pairs(t2) do
-    t1[k] = v
+  local function deepMerge(target, source)
+    for k, v in pairs(source) do
+      if type(v) == "table" and type(target[k]) == "table" then
+        deepMerge(target[k], v)
+      else
+        target[k] = v
+      end
+    end
   end
+  deepMerge(t1, t2)
   return t1
 end
 
