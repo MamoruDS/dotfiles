e80ddc52ef3c3932fa7731ec3b7e4cc771aba9f6
MamoruDS <mamoruds.io@gmail.com>
2024-09-18 14:04:52 +0900

    update(zsh): dark_mode support for fsh plugin

--- zsh/plugins/configs/fsh.zsh
+++ zsh/plugins/configs/fsh.zsh
@@ -1,4 +1,8 @@
-theme_name={{zsh_plugin_fsh_theme}}
+# {{#if (eq dark_mode 1)}}
+theme_name={{zsh_plugin_fsh_theme_dark}}
+# {{else}}
+theme_name={{zsh_plugin_fsh_theme_light}}
+# {{/if}}
 if [ ! -n "$(fast-theme -s | grep "${${theme_name##*/}%.*}")" ]; then
-    fast-theme {{zsh_plugin_fsh_theme}}
+    fast-theme "$theme_name"
 fi
